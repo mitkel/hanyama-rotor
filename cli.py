6557bd620cf4aa05ff5199590176a4f1dc23eb79
mitkel <polaczykb@gmail.com>
2024-02-11 20:54:26 +0100

    remove save path for now

--- cli.py
+++ cli.py
@@ -1,7 +1,5 @@
 import argparse
-from pathlib import Path
 
-from returns.maybe import Some
 from returns.result import Failure, Success
 
 from rotor.explore import find_shortest_path
@@ -9,7 +7,7 @@ from rotor.puzzle import Puzzle
 from rotor.visualize import visualize_available_states
 
 
-def main(initial_state_str: str, final_state_str: str, visualize: bool, save_path: Path) -> None:
+def main(initial_state_str: str, final_state_str: str, visualize: bool) -> None:
     match Puzzle.from_str(initial_state_str), Puzzle.from_str(final_state_str):
         case Success(initial), Success(final):
             initial_state, final_state = initial, final
@@ -21,7 +19,7 @@ def main(initial_state_str: str, final_state_str: str, visualize: bool, save_pat
             return
 
     if visualize:
-        visualize_available_states(initial_state, Some(save_path))
+        visualize_available_states(initial_state)
 
     path = find_shortest_path(initial_state=initial_state, final_state=final_state).value_or([])
     if not path:
@@ -37,7 +35,6 @@ if __name__ == "__main__":
     parser.add_argument("--initial-state", type=str, default=initial_state_str, help="Initial state of the puzzle")
     parser.add_argument("--final-state", type=str, default=final_state_str, help="Final state of the puzzle")
     parser.add_argument("--visualize", action="store_true", help="Visualize the available state space")
-    parser.add_argument("--save-path", type=Path, default=Path(""), help="Path to save the output")
     args = parser.parse_args()
 
-    main(args.initial_state, args.final_state, args.visualize, args.save_path)
+    main(args.initial_state, args.final_state, args.visualize)
